366b7ca87dede6b6bfac09ab8ea8964782de94a3
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-28 21:28:37 +0530

    Navbar setup and install react responsive carousel

--- src/Components/MainLayOut/MainLayOut.jsx
+++ src/Components/MainLayOut/MainLayOut.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Home/Shared/Footer";
+import Navbar from "../Home/Shared/Navbar/Navbar";
 
 
 const MainLayOut = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
