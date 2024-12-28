c4f03d52ec9e22a882b39a7a428f61859121e033
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-28 21:07:28 +0530

    Footer Setup done

--- src/Components/MainLayOut/MainLayOut.jsx
+++ src/Components/MainLayOut/MainLayOut.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Home/Shared/Footer";
 
 
 const MainLayOut = () => {
     return (
         <div>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
