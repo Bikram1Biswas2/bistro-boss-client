b2bc9c52886ab905bd88591f32f9ca5b00aa345e
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-30 00:52:58 +0530

    Show our menu page's category wise all food

--- src/Components/Home/Shared/MenuCategory/MenuCategory.jsx
+++ src/Components/Home/Shared/MenuCategory/MenuCategory.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import Cover from "../Cover/Cover";
+import MenuItem from "../MenuItem/MenuItem";
 
-const MenuCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+const MenuCategory = ({ items, title,img }) => {
+  return (
+    <div>
+      {title && <Cover img={img} title={title}></Cover>}
+      <div className="grid md:grid-cols-2 gap-10 mt-6">
+        {items.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MenuCategory;
--- src/Components/Pages/OurMenu/OurMenu.jsx
+++ src/Components/Pages/OurMenu/OurMenu.jsx
@@ -1,17 +1,39 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Home/Shared/Cover/Cover";
 import img from '../../../assets/menu/banner3.jpg'
-import PopularMenu from "../../Home/PopularMenu/PopularMenu";
+import useMenu from "../../../hooks/useMenu";
+import MenuCategory from "../../Home/Shared/MenuCategory/MenuCategory";
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
+import pizzaImg from "../../../assets/menu/pizza-bg.jpg"
+import saladImg from "../../../assets/menu/salad-bg.jpg"
+import soupImg from "../../../assets/menu/soup-bg.jpg"
+import SectionTitle from "../../Home/SectionTitle/SectionTitle";
 
 
 const OurMenu = () => {
+    const [menu] = useMenu()
+    const offered = menu.filter(item=>item.category==='offered')
+    const dessert = menu.filter(item=>item.category==='dessert')
+    const pizza = menu.filter(item=>item.category==='pizza')
+    const salad = menu.filter(item=>item.category==='salad')
+    const soup = menu.filter(item=>item.category==='soup')
     return (
-        <div>
+        <div className="space-y-8">
             <Helmet>
                 <title>Bistro Boss | Menu</title>
             </Helmet>
            <Cover img={img} title="Our Menu" description="Would You Like To Try A Dish?"></Cover>
-           
+           {/* popular */}
+           <SectionTitle subHeading={"Don't miss "} heading={"todays offer"}></SectionTitle>
+           <MenuCategory items={offered} ></MenuCategory>
+           {/* Dessert */}
+           <MenuCategory items={dessert} img={dessertImg} title={"Desserts"}></MenuCategory>
+           {/* Pizza */}
+           <MenuCategory items={pizza} img={pizzaImg} title={"Pizza"}></MenuCategory>
+           {/* Salad */}
+           <MenuCategory items={salad} img={saladImg} title={"Salad"}></MenuCategory>
+           {/* Soup */}
+           <MenuCategory items={soup} img={soupImg} title={"Soup"}></MenuCategory>
         </div>
     );
 };
