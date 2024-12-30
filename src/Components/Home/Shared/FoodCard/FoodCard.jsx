c1993fc1f67aa69de45ecd333aef51c1b02d5581
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2024-12-30 11:46:58 +0530

    set price for food cards

---
+++ src/Components/Home/Shared/FoodCard/FoodCard.jsx
@@ -0,0 +1,25 @@
+
+
+const FoodCard = ({item}) => {
+    const {name,recipe,image,price}= item
+  
+    return (
+        <div className="card card-compact bg-base-100  shadow-xl">
+        <figure>
+          <img
+            src={image}
+            alt="Shoes" />
+        </figure>
+        <p className="bg-slate-900 text-white absolute right-0 mr-4 mt-4 px-4 py-2 rounded">${price}</p>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>{recipe}</p>
+          <div className="card-actions justify-center">
+            <button className="btn text-yellow-700 border-b-4 border border-current border-t-0 ">Add to Cart</button>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default FoodCard;
--- src/Components/Pages/OrderFood/OrderFood.jsx
+++ src/Components/Pages/OrderFood/OrderFood.jsx
@@ -3,11 +3,21 @@ import orderImg from "../../../assets/shop/order.jpg";
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
 import { useState } from "react";
+import FoodCard from "../../Home/Shared/FoodCard/FoodCard";
+import useMenu from "../../../hooks/useMenu";
+
 
 const OrderFood = () => {
     const [tabIndex,setTabIndex]=useState(0)
+    const [menu] = useMenu()
+    const pizza = menu.filter(item=>item.category==='pizza')
+    const offered = menu.filter(item=>item.category==='offered')
+    const dessert = menu.filter(item=>item.category==='dessert')
+    const salad = menu.filter(item=>item.category==='salad')
+    const soup = menu.filter(item=>item.category==='soup')
+    const drinks = menu.filter(item=>item.category==='drinks')
   return (
-    <div>
+    <div className="space-y-5">
       <Cover
         img={orderImg}
         title={"Order Food"}
@@ -22,9 +32,43 @@ const OrderFood = () => {
           <Tab>Soup</Tab>
           <Tab>Drinks</Tab>
         </TabList>
-        <TabPanel></TabPanel>
-        <TabPanel></TabPanel>
+        <TabPanel>
+            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 ">
+              {
+                dessert.map(item=><FoodCard key={item._id} item={item}></FoodCard>)
+              }
+            </div>
+        </TabPanel>
+        <TabPanel>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {
+                salad.map(item=><FoodCard key={item._id} item={item}></FoodCard>)
+              }
+            </div>
+        </TabPanel>
+        <TabPanel>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {
+                pizza.map(item=><FoodCard key={item._id} item={item}></FoodCard>)
+              }
+            </div>
+        </TabPanel>
+        <TabPanel>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {
+                soup.map(item=><FoodCard key={item._id} item={item}></FoodCard>)
+              }
+            </div>
+        </TabPanel>
+        <TabPanel>
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              {
+                drinks.map(item=><FoodCard key={item._id} item={item}></FoodCard>)
+              }
+            </div>
+        </TabPanel>
       </Tabs>
+     
     </div>
   );
 };
