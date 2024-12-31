f3f104976fb7dcde2ff86529983185d385632e63
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-01 00:57:43 +0530

    Craete AuthProvider and wrap the whole RouterProvider in it

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,16 @@ import "./index.css";
 import { router } from "./Components/Routes/Routes.jsx";
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
+import AuthProvider from "./Provider/AuthProvider.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
-      <div className="max-w-6xl mx-auto">
-        <RouterProvider router={router} />
-      </div>
+      <AuthProvider>
+        <div className="max-w-6xl mx-auto">
+          <RouterProvider router={router} />
+        </div>
+      </AuthProvider>
     </HelmetProvider>
   </StrictMode>
 );
