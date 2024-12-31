3d464f98bfcd84bb860f29c857f23c6932e099af
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-01 02:03:52 +0530

    Craete register page

---
+++ src/Components/Pages/Register/Register.jsx
@@ -0,0 +1,46 @@
+import { Link } from "react-router-dom";
+
+
+const Register = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input type="text" placeholder="Name" name="name" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input type="email" placeholder="email" name="name" className="input input-bordered" required />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input type="password" placeholder="password" name="password" className="input input-bordered" required />
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-secondary">Register</button>
+              </div>
+            </form>
+            <p className="text-center p-5"><small>Already Register?Please <Link to='/login' className="text-green-600">Login</Link></small></p>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Register;
