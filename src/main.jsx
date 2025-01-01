0b37ea827915e4838fe840265c71ea2dfe53da37
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-02 01:22:24 +0530

    install tanstack query and routerProvider wraps in the QueryClientProvider

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,22 @@ import { router } from "./Components/Routes/Routes.jsx";
 import { RouterProvider } from "react-router-dom";
 import { HelmetProvider } from "react-helmet-async";
 import AuthProvider from "./Provider/AuthProvider.jsx";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <HelmetProvider>
       <AuthProvider>
-        <div className="max-w-6xl mx-auto">
-          <RouterProvider router={router} />
-        </div>
+        <QueryClientProvider client={queryClient}>
+          <div className="max-w-6xl mx-auto">
+            <RouterProvider router={router} />
+          </div>
+        </QueryClientProvider>
       </AuthProvider>
     </HelmetProvider>
   </StrictMode>
