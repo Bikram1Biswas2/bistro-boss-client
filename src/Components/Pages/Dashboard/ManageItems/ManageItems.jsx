f63670b5c04fcc08b8eb37da460ca6643e570cb2
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-08 15:00:40 +0530

    set section title component for manageItems in admin dashboard

---
+++ src/Components/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -0,0 +1,12 @@
+import SectionTitle from "../../../Home/SectionTitle/SectionTitle";
+
+
+const ManageItems = () => {
+    return (
+        <div>
+            <SectionTitle heading='Manage All Items' subHeading='Hurry Up'></SectionTitle>
+        </div>
+    );
+};
+
+export default ManageItems;
