232835dc3c3d45f3e9f312acd382ad66c8b042dd
Bikram1Biswas2 <bikrambiswas912@gmail.com>
2025-01-08 16:22:38 +0530

    use TanstackQuery instead of fetch to load MenuItems

--- src/Components/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/Components/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -2,12 +2,38 @@ import { FaTrash } from "react-icons/fa";
 import useMenu from "../../../../hooks/useMenu";
 import SectionTitle from "../../../Home/SectionTitle/SectionTitle";
 import { TfiWrite } from "react-icons/tfi";
+import Swal from "sweetalert2";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 const ManageItems = () => {
     const [menu] = useMenu();
+    const axiosSecure = useAxiosSecure()
 
     const handleDeleteItem = (item)=>{
-
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+          }).then(async(result) => {
+            if (result.isConfirmed) {
+                const result = await axiosSecure.delete(`/menu/${item._id}`)
+                console.log(result.data);
+                 if(result.data.deletedCount>0){
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${item.name} has been deleted successfully`,
+                        showConfirmButton: false,
+                        timer: 1500
+                      });
+                 }
+                
+            }
+          });
     }
 
     return (
--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,16 +1,29 @@
+import { useQuery } from "@tanstack/react-query";
 import { useEffect, useState } from "react";
+import useAxiosSecure from "./useAxiosSecure";
 
 const useMenu = () => {
-  const [menu, setMenu] = useState([]);
-  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/menu")
-      .then((res) => res.json())
-      .then((data) => {
-        setMenu(data);
-      });
-  }, []);
+  const axiosSecure = useAxiosSecure()
+
+  // const [menu, setMenu] = useState([]);
+  // const [loading, setLoading] = useState(true);
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/menu")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setMenu(data);
+  //     });
+  // }, []);
+
+  const {data: menu=[],isPending: loading,refetch} = useQuery({
+    queryKey:['menu'],
+    queryFn: async()=>{
+      const res = await axiosSecure.get('/menu')
+      return res.data
+    }
+  })
+
   return [menu, loading];
 };
 
